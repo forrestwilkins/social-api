f10b7ba1d3cde5d88157209c34d3e3fe5bdc0208
forrestwilkins <ethanforrestwilkins@gmail.com>
2022-09-27 23:37:28 -0400

    refactor: added _createImagesLoader method to dataloader service

--- src/dataloader/dataloader.service.ts
+++ src/dataloader/dataloader.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from "@nestjs/common";
 import * as DataLoader from "dataloader";
-import { createImagesLoader } from "../images/images.loader";
+import { In } from "typeorm";
 import { ImagesService } from "../images/images.service";
 import { Image } from "../images/models/image.model";
 
@@ -13,7 +13,21 @@ export class DataloaderService {
   constructor(private imagesService: ImagesService) {}
 
   getLoaders(): Dataloaders {
-    const imagesLoader = createImagesLoader(this.imagesService);
+    const imagesLoader = this._createImagesLoader();
     return { imagesLoader };
+  }
+
+  _createImagesLoader() {
+    return new DataLoader<number, Image[]>(async (postIds) => {
+      const images = await this.imagesService.getImages({
+        postId: In(postIds as number[]),
+      });
+      const mappedImages = postIds.map(
+        (id) =>
+          images.filter((image: Image) => image.postId === id) ||
+          new Error(`Could not load image ${id}`)
+      );
+      return mappedImages;
+    });
   }
 }
