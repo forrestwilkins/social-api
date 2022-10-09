4753bc70347106b7f93ec77791b51b652aabb431
forrestwilkins <ethanforrestwilkins@gmail.com>
2022-10-08 20:53:06 -0400

    feat: added group and groups queries for single and all

--- src/groups/groups.resolver.ts
+++ src/groups/groups.resolver.ts
@@ -1,4 +1,18 @@
-import { Resolver } from "@nestjs/graphql";
+import { Args, ID, Query, Resolver } from "@nestjs/graphql";
+import { Group } from "./group.model";
+import { GroupsService } from "./groups.service";
 
-@Resolver()
-export class GroupsResolver {}
+@Resolver(() => Group)
+export class GroupsResolver {
+  constructor(private groupsService: GroupsService) {}
+
+  @Query(() => Group)
+  async group(@Args("id", { type: () => ID }) id: number) {
+    return this.groupsService.getGroup(id);
+  }
+
+  @Query(() => [Group])
+  async groups() {
+    return this.groupsService.getGroups();
+  }
+}
--- src/groups/groups.service.ts
+++ src/groups/groups.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { FindOptionsWhere, Repository } from "typeorm";
+import { Group } from "./group.model";
 
 @Injectable()
-export class GroupsService {}
+export class GroupsService {
+  constructor(
+    @InjectRepository(Group)
+    private repository: Repository<Group>
+  ) {}
+
+  async getGroup(id: number) {
+    return this.repository.findOne({ where: { id } });
+  }
+
+  async getGroups(where?: FindOptionsWhere<Group>) {
+    return this.repository.find({ where, order: { createdAt: "DESC" } });
+  }
+}
