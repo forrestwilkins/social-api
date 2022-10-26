dff2b2dbbb7e42cdaed973f7d7f6546ea9fd8436
forrestwilkins <ethanforrestwilkins@gmail.com>
2022-10-25 20:11:12 -0400

    feat: added model imports for group member and member request modules

--- src/groups/group-members/group-members.module.ts
+++ src/groups/group-members/group-members.module.ts
@@ -1,8 +1,11 @@
 import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
 import { GroupMembersResolver } from "./group-members.resolver";
 import { GroupMembersService } from "./group-members.service";
+import { GroupMember } from "./models/group-member.model";
 
 @Module({
+  imports: [TypeOrmModule.forFeature([GroupMember])],
   providers: [GroupMembersResolver, GroupMembersService],
 })
 export class GroupMembersModule {}
--- src/groups/member-requests/member-requests.module.ts
+++ src/groups/member-requests/member-requests.module.ts
@@ -1,8 +1,11 @@
 import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
 import { MemberRequestsResolver } from "./member-requests.resolver";
 import { MemberRequestsService } from "./member-requests.service";
+import { MemberRequest } from "./models/member-request.model";
 
 @Module({
+  imports: [TypeOrmModule.forFeature([MemberRequest])],
   providers: [MemberRequestsResolver, MemberRequestsService],
 })
 export class MemberRequestsModule {}
