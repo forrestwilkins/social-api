3d167daccc40acc94dfd66853e45852f3c6d85a0
forrestwilkins <ethanforrestwilkins@gmail.com>
2022-10-25 20:18:28 -0400

    feat: added groupMembers query

--- src/groups/group-members/group-members.resolver.ts
+++ src/groups/group-members/group-members.resolver.ts
@@ -1,4 +1,13 @@
-import { Resolver } from "@nestjs/graphql";
+import { Query, Resolver } from "@nestjs/graphql";
+import { GroupMembersService } from "./group-members.service";
+import { GroupMember } from "./models/group-member.model";
 
 @Resolver()
-export class GroupMembersResolver {}
+export class GroupMembersResolver {
+  constructor(private service: GroupMembersService) {}
+
+  @Query(() => [GroupMember])
+  async groupMembers() {
+    return this.service.getGroupMembers();
+  }
+}
--- src/groups/group-members/group-members.service.ts
+++ src/groups/group-members/group-members.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { FindOptionsWhere, Repository } from "typeorm";
+import { GroupMember } from "./models/group-member.model";
 
 @Injectable()
-export class GroupMembersService {}
+export class GroupMembersService {
+  constructor(
+    @InjectRepository(GroupMember)
+    private repository: Repository<GroupMember>
+  ) {}
+
+  async getGroupMembers(where?: FindOptionsWhere<GroupMember>) {
+    return this.repository.find({ where, order: { createdAt: "DESC" } });
+  }
+}
