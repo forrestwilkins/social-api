54b5ef109d6923582fdc775e4088374db0561f8d
forrestwilkins <ethanforrestwilkins@gmail.com>
2022-11-22 19:54:54 -0500

    refactor: removed unused groupMembers query from GroupMembersResolver

--- src/groups/group-members/group-members.resolver.ts
+++ src/groups/group-members/group-members.resolver.ts
@@ -1,25 +1,11 @@
-import {
-  Context,
-  Parent,
-  Query,
-  ResolveField,
-  Resolver,
-} from "@nestjs/graphql";
+import { Context, Parent, ResolveField, Resolver } from "@nestjs/graphql";
 import { Dataloaders } from "../../dataloader/dataloader.service";
 import { User } from "../../users/models/user.model";
 import { Group } from "../models/group.model";
-import { GroupMembersService } from "./group-members.service";
 import { GroupMember } from "./models/group-member.model";
 
 @Resolver(() => GroupMember)
 export class GroupMembersResolver {
-  constructor(private service: GroupMembersService) {}
-
-  @Query(() => [GroupMember])
-  async groupMembers() {
-    return this.service.getGroupMembers();
-  }
-
   @ResolveField(() => User)
   async user(
     @Context() { loaders }: { loaders: Dataloaders },
