b9e895fb4a6a848b40276dfbd5d7f94a0f76ea33
forrestwilkins <ethanforrestwilkins@gmail.com>
2022-11-29 14:53:35 -0500

    feat: install graphql-shield, add permissions boilerplate

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,9 @@ import { ApolloDriver, ApolloDriverConfig } from "@nestjs/apollo";
 import { Module } from "@nestjs/common";
 import { GraphQLModule } from "@nestjs/graphql";
 import { TypeOrmModule } from "@nestjs/typeorm";
+import { GraphQLSchema } from "graphql";
+import { applyMiddleware } from "graphql-middleware";
+import { rule, shield } from "graphql-shield";
 import { AuthModule } from "./auth/auth.module";
 import { DataloaderModule } from "./dataloader/dataloader.module";
 import { DataloaderService } from "./dataloader/dataloader.service";
@@ -11,6 +14,21 @@ import ormconfig from "./ormconfig";
 import { PostsModule } from "./posts/posts.module";
 import { RolesModule } from "./roles/roles.module";
 import { UsersModule } from "./users/users.module";
+
+// TODO: Add logic for checking auth state
+const isAuthenticated = rule()(async () => true);
+
+// TODO: Add remaining permissions and move to own file
+const permissions = shield(
+  {
+    Query: {
+      users: isAuthenticated,
+    },
+  },
+  {
+    fallbackError: "Unauthorized",
+  }
+);
 
 @Module({
   imports: [
@@ -26,6 +44,10 @@ import { UsersModule } from "./users/users.module";
         context: () => ({
           loaders: dataloaderService.getLoaders(),
         }),
+        transformSchema: (schema: GraphQLSchema) => {
+          schema = applyMiddleware(schema, permissions);
+          return schema;
+        },
       }),
     }),
     AuthModule,
