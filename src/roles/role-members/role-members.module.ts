b52cd3dd30701d717862bace4c7fab532b44ef6e
forrestwilkins <ethanforrestwilkins@gmail.com>
2022-12-03 18:47:04 -0500

    feat: add TypeOrmModule for RoleMember

--- src/roles/role-members/role-members.module.ts
+++ src/roles/role-members/role-members.module.ts
@@ -1,8 +1,11 @@
 import { Module } from "@nestjs/common";
-import { RoleMembersService } from "./role-members.service";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { RoleMember } from "./models/role-member.model";
 import { RoleMembersResolver } from "./role-members.resolver";
+import { RoleMembersService } from "./role-members.service";
 
 @Module({
+  imports: [TypeOrmModule.forFeature([RoleMember])],
   providers: [RoleMembersService, RoleMembersResolver],
 })
 export class RoleMembersModule {}
