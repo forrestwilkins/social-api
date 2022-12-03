65eca4b298b6970b5d7e6c6ab2dc5aef9ebb53fd
forrestwilkins <ethanforrestwilkins@gmail.com>
2022-12-03 18:53:21 -0500

    feat: add getRoleMember and getRoleMembers service methods

--- src/roles/role-members/role-members.service.ts
+++ src/roles/role-members/role-members.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { FindOptionsWhere, Repository } from "typeorm";
+import { RoleMember } from "./models/role-member.model";
 
 @Injectable()
-export class RoleMembersService {}
+export class RoleMembersService {
+  constructor(
+    @InjectRepository(RoleMember)
+    private repository: Repository<RoleMember>
+  ) {}
+
+  async getRoleMember(id: number) {
+    return this.repository.findOne({ where: { id } });
+  }
+
+  async getRoleMembers(where?: FindOptionsWhere<RoleMember>) {
+    return this.repository.find({ where });
+  }
+}
