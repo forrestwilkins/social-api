0563a68868eb043e01a1424734a3dbb252f58590
forrestwilkins <ethanforrestwilkins@gmail.com>
2022-12-04 19:09:37 -0500

    refactor: removed unused hasPermissions rule

--- src/auth/shield.ts
+++ src/auth/shield.ts
@@ -1,21 +1,11 @@
 // TODO: Add remaining permissions and logic for checking auth state
 
 import { allow, rule, shield } from "graphql-shield";
-import { Context } from "../shared/shared.types";
 import { UNAUTHORIZED } from "../shared/shared.constants";
+import { Context } from "../shared/shared.types";
 
 const isAuthenticated = rule({ cache: "contextual" })(
   async (_, __, ctx: Context) => !!ctx.permissions
-);
-
-const hasPermission = rule({ cache: "contextual" })(
-  async (_, __, ctx: Context) => {
-    // TODO: Remove when no longer needed for testing
-    console.log(ctx.permissions);
-    console.log(ctx.user);
-
-    return true;
-  }
 );
 
 const shieldPermissions = shield(
@@ -23,7 +13,6 @@ const shieldPermissions = shield(
     Query: {
       authCheck: isAuthenticated,
       me: isAuthenticated,
-      posts: hasPermission,
       users: isAuthenticated,
     },
     Mutation: {
