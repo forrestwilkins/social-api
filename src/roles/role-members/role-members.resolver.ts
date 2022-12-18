13738fb1381b6396f565f214cb4a03b510aabd32
forrestwilkins <ethanforrestwilkins@gmail.com>
2022-12-18 12:55:40 -0500

    feat: add user field resolver for role members

--- src/roles/role-members/role-members.resolver.ts
+++ src/roles/role-members/role-members.resolver.ts
@@ -1,4 +1,15 @@
-import { Resolver } from "@nestjs/graphql";
+import { Context, Parent, ResolveField, Resolver } from "@nestjs/graphql";
+import { Dataloaders } from "../../dataloader/dataloader.service";
+import { User } from "../../users/models/user.model";
+import { RoleMember } from "./models/role-member.model";
 
-@Resolver()
-export class RoleMembersResolver {}
+@Resolver(() => RoleMember)
+export class RoleMembersResolver {
+  @ResolveField(() => User)
+  async user(
+    @Context() { loaders }: { loaders: Dataloaders },
+    @Parent() { userId }: RoleMember
+  ) {
+    return loaders.usersLoader.load(userId);
+  }
+}
