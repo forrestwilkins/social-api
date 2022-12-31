b89db552b933330ea741817301b645cabccc270e
forrestwilkins <ethanforrestwilkins@gmail.com>
2022-12-31 09:40:35 -0500

    feat: add create proposal mutation resolver

--- src/proposals/proposals.resolver.ts
+++ src/proposals/proposals.resolver.ts
@@ -1,5 +1,20 @@
-import { Resolver } from "@nestjs/graphql";
+import { Args, Mutation, Resolver } from "@nestjs/graphql";
+import { CurrentUser } from "../auth/decorators/current-user.decorator";
+import { User } from "../users/models/user.model";
+import { CreateProposalInput } from "./models/create-proposal.input";
+import { CreateProposalPayload } from "./models/create-proposal.payload";
 import { Proposal } from "./models/proposal.model";
+import { ProposalsService } from "./proposals.service";
 
 @Resolver(() => Proposal)
-export class ProposalsResolver {}
+export class ProposalsResolver {
+  constructor(private proposalsService: ProposalsService) {}
+
+  @Mutation(() => CreateProposalPayload)
+  async createProposal(
+    @Args("proposalData") proposalData: CreateProposalInput,
+    @CurrentUser() user: User
+  ) {
+    return this.proposalsService.createProposal(user, proposalData);
+  }
+}
--- src/proposals/proposals.service.ts
+++ src/proposals/proposals.service.ts
@@ -1,4 +1,37 @@
 import { Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { FindOptionsWhere, Repository } from "typeorm";
+import { User } from "../users/models/user.model";
+import { CreateProposalInput } from "./models/create-proposal.input";
+import { Proposal } from "./models/proposal.model";
 
 @Injectable()
-export class ProposalsService {}
+export class ProposalsService {
+  constructor(
+    @InjectRepository(Proposal)
+    private repository: Repository<Proposal>
+  ) {}
+
+  async getProposal(id: number) {
+    return this.repository.findOne({ where: { id } });
+  }
+
+  async getProposals(where?: FindOptionsWhere<Proposal>) {
+    return this.repository.find({ where, order: { createdAt: "DESC" } });
+  }
+
+  async createProposal(
+    user: User,
+    { images, ...proposalData }: CreateProposalInput
+  ) {
+    const proposal = await this.repository.save({
+      ...proposalData,
+      userId: user.id,
+    });
+
+    // TODO: Remove when no longer needed for testing
+    console.log(images);
+
+    return { proposal };
+  }
+}
