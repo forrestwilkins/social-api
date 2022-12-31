ef853534338661842aba334f1ffc644da38e90f4
forrestwilkins <ethanforrestwilkins@gmail.com>
2022-12-30 19:02:44 -0500

    feat: import vote and proposal models for respective modules

--- src/proposals/proposals.module.ts
+++ src/proposals/proposals.module.ts
@@ -1,8 +1,11 @@
 import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { Proposal } from "./models/proposal.model";
 import { ProposalsResolver } from "./proposals.resolver";
 import { ProposalsService } from "./proposals.service";
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Proposal])],
   providers: [ProposalsService, ProposalsResolver],
 })
 export class ProposalsModule {}
--- src/votes/votes.module.ts
+++ src/votes/votes.module.ts
@@ -1,8 +1,11 @@
 import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { Vote } from "./models/vote.model";
 import { VotesResolver } from "./votes.resolver";
 import { VotesService } from "./votes.service";
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Vote])],
   providers: [VotesService, VotesResolver],
 })
 export class VotesModule {}
