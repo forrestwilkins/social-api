5e2469112ce1358d053dd67a123cb152885cbbae
forrestwilkins <ethanforrestwilkins@gmail.com>
2023-01-19 21:47:31 -0500

    refactor: remove unused proposal-actions.resolver

--- src/proposals/proposal-actions/proposal-actions.module.ts
+++ src/proposals/proposal-actions/proposal-actions.module.ts
@@ -1,12 +1,11 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { ProposalAction } from "./models/proposal-action.model";
-import { ProposalActionsResolver } from "./proposal-actions.resolver";
 import { ProposalActionsService } from "./proposal-actions.service";
 
 @Module({
   imports: [TypeOrmModule.forFeature([ProposalAction])],
-  providers: [ProposalActionsService, ProposalActionsResolver],
+  providers: [ProposalActionsService],
   exports: [ProposalActionsService],
 })
 export class ProposalActionsModule {}
