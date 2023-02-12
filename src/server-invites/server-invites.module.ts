443b694d3eb7bd8cf713b52df2fba229a0b799c0
forrestwilkins <ethanforrestwilkins@gmail.com>
2023-02-12 18:56:12 -0500

    feat: import server invites model into module

--- src/server-invites/server-invites.module.ts
+++ src/server-invites/server-invites.module.ts
@@ -1,8 +1,11 @@
 import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { ServerInvite } from "./models/server-invite.model";
 import { ServerInvitesResolver } from "./server-invites.resolver";
 import { ServerInvitesService } from "./server-invites.service";
 
 @Module({
+  imports: [TypeOrmModule.forFeature([ServerInvite])],
   providers: [ServerInvitesResolver, ServerInvitesService],
 })
 export class ServerInvitesModule {}
